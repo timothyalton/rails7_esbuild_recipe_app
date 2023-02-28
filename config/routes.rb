5e06639afedd2014d6b84667743a58d4ba79efbe
timothyalton <timothy.alton@gmail.com>
2023-02-28 13:50:28 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      get 'recipes/create'
+      get 'recipes/show'
+      get 'recipes/destroy'
+    end
+  end
+  root 'homepage#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
