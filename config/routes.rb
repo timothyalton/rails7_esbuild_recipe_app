8ddddce5e8c811f2674783b72b79f5c251889abd
timothyalton <timothy.alton@gmail.com>
2023-03-01 01:44:40 -0600

    added recipe controller. added routes. created seed data.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'recipes/index'
-      get 'recipes/create'
-      get 'recipes/show'
-      get 'recipes/destroy'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
     end
   end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
